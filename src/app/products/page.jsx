8aca41b1bfbd10a1c99f6f14219d0cd1b47a4585
whetty <148314559+whetty@users.noreply.github.com>
2024-07-15 14:37:14 +0300

    routes to the e-commerce app

---
+++ src/app/products/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <>
+    <h2 class="text-center p-5 font-cave text-9xl">Products.</h2>
+    <p class="text-center p-5 font-cave text-3xl">This page contains all the products. Currently our last minute low budget store has no products.</p>
+    </>
+  )
+}
+
+export default page
