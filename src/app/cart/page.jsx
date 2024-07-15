bf8e1ff5e58fcdb93d826b27b7db25498f6145b4
whetty <148314559+whetty@users.noreply.github.com>
2024-07-15 14:38:05 +0300

    routes to the e-commerce app

---
+++ src/app/cart/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <>
+    <h2 class="text-center p-5 font-cave text-9xl">Shopping Cart</h2>
+    <p class="text-center p-5 font-cave text-3xl">Your shopping cart is empty. Please head back to the products page and buy a product. Please I'm begging you.</p>
+    </>
+  )
+}
+
+export default page
