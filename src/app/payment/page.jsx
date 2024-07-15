dfe83efe653660cd4063329a0c3902ce59f401ee
whetty <148314559+whetty@users.noreply.github.com>
2024-07-15 14:39:13 +0300

    routes to the e-commerce app

---
+++ src/app/payment/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <>
+    <h2 class="text-center p-5 font-cave text-9xl">Payment</h2>
+    <p class="text-center p-5 font-cave text-3xl">Your payment could not be processed. (I wonder why.)</p>
+    </>
+  )
+}
+
+export default page
